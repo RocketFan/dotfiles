e12a3d4446f4d49f049f15ce073fcfbe88505dfb
RocketFan <kubal123.tomczak@gmail.com>
2023-02-03 13:10:20 +0100

    Add default screenlayout to i3

--- nvim/init.lua
+++
@@ -1,2 +0,0 @@
-require "user.options"
-require "user.keymaps"
---
+++ screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-3 --off --output HDMI-0 --mode 1920x1080 --pos 1680x0 --rotate normal --output DP-4 --off --output DP-5 --off
