19a1bbbfe76c8d5e97a1d2bc24b50c30e0add01d
RocketFan <kubal123.tomczak@gmail.com>
2023-03-25 14:57:09 +0100

    Add screen shoot shortcuts in i3wm

--- screenlayout/default.sh
+++ screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --mode 1680x1050 --pos 0x0 --rotate normal --output DP-3 --off --output HDMI-0 --mode 1920x1080 --pos 1680x0 --rotate normal --output DP-4 --off --output DP-5 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output HDMI-1 --off --output DP-1 --off --output DP-2 --off --output DP-2-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2-2 --off --output DP-2-3 --primary --mode 1920x1080 --pos 0x0 --rotate normal
