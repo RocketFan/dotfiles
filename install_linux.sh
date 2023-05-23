8e715b4a74346f4b5a784cb9a74d163e38de0e05
RocketFan <kubal123.tomczak@gmail.com>
2023-05-23 12:55:55 +0200

    Color alias for ssh

--- install_linux.sh
+++ install_linux.sh
@@ -97,3 +97,4 @@ conda config --set auto_activate_base false
 echo 'export GST_PLUGIN_SCANNER=/usr/lib/x86_64-linux-gnu/gstreamer1.0/gstreamer-1.0/gst-plugin-scanner' >> ~/.bashrc
 echo 'export GST_PLUGIN_PATH=/usr/lib/x86_64-linux-gnu/gstreamer-1.0' >> ~/.bashrc
 echo 'export GST_PLUGIN_SYSTEM_PATH=/usr/lib/x86_64-linux-gnu/gstreamer-1.0' >> ~/.bashrc
+echo 'alias ssh='TERM=xterm-256color ssh'' >> ~/.bashrc
