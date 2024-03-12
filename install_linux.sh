c8e831dbb2421a9668c6fe084238202129e0df86
RocketFan <kubal123.tomczak@gmail.com>
2024-03-12 10:48:46 +0100

    Vi mode in zsh

--- install_linux.sh
+++ install_linux.sh
@@ -94,7 +94,8 @@ setxkbmap -layout pl
 
 sudo apt install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
 
 ##############
 # Mini Conda #
--- zshrc
+++ zshrc
@@ -1,10 +1,14 @@
 export ZSH="$HOME/.oh-my-zsh"
+export VI_MODE_SET_CURSOR=true
+# export KEYTIMEOUT=1
 
 ZSH_THEME="bira"
 
-plugins=(git z zsh-autosuggestions)
+plugins=(git z zsh-autosuggestions zsh-vi-mode)
 
 source $ZSH/oh-my-zsh.sh
+
+alias vim="nvim"
 
 function acp() {
   git add .
