f3f4649bea5cd67452a4d7eac037c75779de7f25
RocketFan <kubal123.tomczak@gmail.com>
2024-04-10 14:41:44 +0200

    Install obsidian and syncthing

--- install/apps.sh
+++ install/apps.sh
@@ -4,7 +4,14 @@ set -e
 cd /tmp
 
 sudo snap install code --classic
+sudo snap install obsidian --classic
 sudo snap install discord
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+sudo apt install software-properties-common apt-transport-https wget curl -y
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update
+sudo apt install syncthing -y
